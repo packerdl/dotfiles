0d8f410ad0573dc99e64662b4c450a87877965b4
Devin Packer <6426163+packerdl@users.noreply.github.com>
2017-11-09 21:49:43 -0500

    Polybar now launches on each monitor.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate existing instances of polybar
 killall -q polybar
@@ -6,6 +6,10 @@ killall -q polybar
 # Wait until processes have shut down fully
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars
-polybar top &
+# Display on each monitor
+monitors=($(xrandr -q | grep " connected" | cut -d ' ' -f 1))
+for monitor in ${monitors[@]}; do
+  MONITOR=$monitor polybar top -r &
+  echo -e "\e[1m\e[32m[DONE]\e[39m\e[0m Polybar lauched on $monitor..."
+done
 
