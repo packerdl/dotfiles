c74b1ddb9ac06ff47d23429bec3e0d5150384136
Devin Packer <6426163+packerdl@users.noreply.github.com>
2017-11-24 10:56:49 -0500

    Compute bar width by fetching resolution and subtracting bspwm padding.

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -14,7 +14,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Display on each monitor
 monitors=($(xrandr -q | grep " connected" | cut -d ' ' -f 1))
 for monitor in ${monitors[@]}; do
-  MONITOR=$monitor polybar top -r &
+  screen_width=$(xrandr | grep "^$monitor" | grep -oP "[0-9]+(?=x)")
+  # Subtract 2x bspwm window gap to bar width
+  bar_width=$(($screen_width-32))
+  BAR_WIDTH=$bar_width MONITOR=$monitor polybar top -r &
   echo -e "\e[1m\e[32m[DONE]\e[39m\e[0m Polybar lauched on $monitor..."
 done
 
