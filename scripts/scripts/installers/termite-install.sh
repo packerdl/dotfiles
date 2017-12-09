df36b337ff9c8fd02bbea8c699d64dc905d62a36
Devin Packer <6426163+packerdl@users.noreply.github.com>
2017-12-09 09:47:59 -0500

    scripts: Create scripts folder. Move termite install script there.

---
+++ scripts/scripts/installers/termite-install.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env sh
+# Copyright (C) 2007 Guillaume Doré <https://github.com/Corwind/termite-install>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+git clone --recursive https://github.com/thestinger/termite.git
+git clone https://github.com/thestinger/vte-ng.git
+sudo apt-get install -y \
+	g++ \
+	libgtk-3-dev \
+	gtk-doc-tools \
+	gnutls-bin \
+	valac \
+	intltool \
+	libpcre2-dev \
+	libglib3.0-cil-dev \
+	libgnutls28-dev \
+	libgirepository1.0-dev \
+	libxml2-utils \
+	gperf
+
+echo export LIBRARY_PATH="/usr/include/gtk-3.0:$LIBRARY_PATH"
+cd vte-ng && ./autogen.sh && make && sudo make install
+cd ../termite && make && sudo make install
+sudo ldconfig
+sudo mkdir -p /lib/terminfo/x; sudo ln -s \
+/usr/local/share/terminfo/x/xterm-termite \
+/lib/terminfo/x/xterm-termite
