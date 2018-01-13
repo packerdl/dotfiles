3f0247e8409005cddf7839bd36568f3ef4eb0805
Devin Packer <6426163+packerdl@users.noreply.github.com>
2018-01-12 19:25:38 -0500

    polybar: Better colors for bspwm module. Make bar full width.

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -17,7 +17,9 @@ for monitor in ${monitors[@]}; do
   screen_width=$(xrandr | grep "^$monitor" | grep -oP "[0-9]+(?=x)")
   # Subtract 2x bspwm window gap to bar width
   bar_width=$(($screen_width-32))
-  BAR_WIDTH=$bar_width MONITOR=$monitor polybar top -r &
-  echo -e "\e[1m\e[32m[DONE]\e[39m\e[0m Polybar lauched on $monitor..."
+  BAR_WIDTH=$bar_width \
+    MONITOR=$monitor \
+    SCREEN_WIDTH=$screen_width \
+    polybar top -r &
+  echo -e "\e[1m\e[32m[DONE]\e[39m\e[0m Polybar launched on $monitor..."
 done
-
