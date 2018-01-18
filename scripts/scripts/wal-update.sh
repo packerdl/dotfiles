8b1f0ac1a525605319c9fe8f2b4a3e905c457258
Devin Packer <6426163+packerdl@users.noreply.github.com>
2018-01-17 21:32:05 -0500

    rofi: Apply transparent variant of wal color scheme.

--- scripts/scripts/wal-update.sh
+++ scripts/scripts/wal-update.sh
@@ -1,8 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 if command -V bspc > /dev/null; then
   . "$HOME/.cache/wal/colors.sh"
   bspc config normal_border_color $background
   bspc config focused_border_color $foreground
   bspc config active_border_color $color8
   bspc config presel_feedback_color $foreground
+fi
+
+if command -V rofi > /dev/null; then
+  echo "rofi.color-window: #C0${background:1:6}, #C0${background:1:6}, ${color1}" | xrdb -merge
+  echo "rofi.color-normal: #00000000, ${foreground}, #00000000, ${color2}, ${background}" | xrdb -merge
+  echo "rofi.color-active: #00000000, ${foreground}, #00000000, ${color2}, ${background}" | xrdb -merge
+  echo "rofi.color-urgent: #00000000, ${color1}, #00000000, ${color1}, $foreground" | xrdb -merge
 fi
